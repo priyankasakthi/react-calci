a288c50a33a8582c4af5cce56d016690e24a31be
Priyanka S <prisakt1@in.ibm.com>
2019-08-21 12:16:19 +0530

    Updates

---
+++ precache-manifest.64727dcccd3de2822308db693d76c539.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc1054e19129426b836b6faa8892b4b",
+    "url": "/react-calci/index.html"
+  },
+  {
+    "revision": "04aa741b4f26d6ed573c",
+    "url": "/react-calci/static/css/2.d527c19d.chunk.css"
+  },
+  {
+    "revision": "de2f749c408e5b98929d",
+    "url": "/react-calci/static/css/main.dcaa98f7.chunk.css"
+  },
+  {
+    "revision": "04aa741b4f26d6ed573c",
+    "url": "/react-calci/static/js/2.5c433d01.chunk.js"
+  },
+  {
+    "revision": "de2f749c408e5b98929d",
+    "url": "/react-calci/static/js/main.1046859c.chunk.js"
+  },
+  {
+    "revision": "205e076d9a4ac6cf9583",
+    "url": "/react-calci/static/js/runtime~main.ce2d6053.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-calci/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-calci/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-calci/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-calci/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-calci/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
